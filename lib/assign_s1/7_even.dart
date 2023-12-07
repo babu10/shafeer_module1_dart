cab20d3d5be3c564e07106bb82f613ee081be9f9
Vijayleo46 <vijaymartin72@gmail.com>
2023-12-07 17:06:41 +0530

    done

---
+++ lib/assign_s1/7_even.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  int a=2;
+  if(a%2==0) {
+    print("Even");
+  }
+
+  else
+    {
+      print("You entered Odd");
+    }
+}
---
+++ lib/assign_s1/8_odd_print.dart
@@ -0,0 +1,13 @@
+//Write a program for
+//Printing Odd numbers between 1 and 50
+void main()
+{
+  for(int i=1;i<=50;i++)
+    {
+      if(i%2!=0)
+        {
+          print(i);
+        }
+    }
+
+}
