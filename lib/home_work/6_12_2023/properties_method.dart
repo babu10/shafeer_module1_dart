2ec4b634e9c141560b6de6c3617bfeea605496fe
saf <kololamba@gmail.com>
2023-12-07 19:01:32 +0530

    daily work

---
+++ lib/home_work/6_12_2023/properties_method.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  //properties String
+ /* String className="MAllu";
+  print(className.isEmpty);
+  print(className.length);
+  //Method/function-->() String
+  print(className.toLowerCase());
+  print(className.toUpperCase());*/
+
+
+
+ //Properties Int
+ double number1=10.6;
+// print(number1.isEven);
+ print(number1.isNegative);
+ //Method
+ print(number1.round());
+}
