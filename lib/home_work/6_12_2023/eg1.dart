e7272341e0e4be667ce2208cbb28d2bb9f89c7d0
saf <kololamba@gmail.com>
2023-12-13 19:14:43 +0530

    daily work

--- lib/home_work/6_12_2023/eg1.dart
+++ lib/home_work/6_12_2023/eg1.dart
@@ -1,7 +1,13 @@
-import'dart:io';
-void main()
-{
-  print("Enter one number");
-  int? a=int.parse(stdin.readLineSync()!);
-  print(a);
+void main() {
+  final int x = 10;  // Assignable at runtime
+  const int y = 20;  // Must be a compile-time constant
+
+  // Using final
+  final int z = x + 5;  // Allowed because 'x' is final
+
+  // Using const
+  const int result = 9 + 5; // This would be an error because 'x' is not a compile-time constant
+
+  print(z);
+  print(y);
 }
